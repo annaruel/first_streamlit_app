3e1b13abd905951a5783a019ef38cc6e9ad86e04
annaruel <45943333+annaruel@users.noreply.github.com>
2023-01-23 19:44:04 -0500

    Update streamlit_app.py
    pasting in lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My parents new healthy diner')
 streamlit.title('What next?')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Hard-boiled Free-Range Egg')
-streamlit.text('Omega-3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
