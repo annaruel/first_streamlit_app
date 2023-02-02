c7a6806b251eef271a645dc59d19b5034f094506
annaruel <45943333+annaruel@users.noreply.github.com>
2023-02-02 09:36:57 -0500

    Update streamlit_app.py
    remove default fruit to add

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 #Allow the user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'boo')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 
